6f6bd71ec9c5dc2b7f8bf13d9e965c6bade80048
itu-itis21-beyden20 <beyden20@itu.edu.tr>
2023-11-27 00:34:32 +0300

    random user
    sport table sql kodu ve user generating python kodu

---
+++ database/create_db/sport_table.sql
@@ -0,0 +1,11 @@
+--insert sport table their values
+INSERT INTO sport (sport_id, sport_type, is_competitive, capacity_min, capacity_max)
+VALUES 
+(1,'Futbol', True, 5, 11), 
+(2,'Voleybol', True, 2, 6), 
+(3,'Basketbol', True, 1, 5),
+(4,'Tenis', True, 1, 2),
+(5,'Yüzme', False, 1, 1),
+(6,'Gym', False, 1, 1),
+(7,'Ping-Pong', True, 2, 2);
+

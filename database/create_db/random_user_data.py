d4aa00000af2dd8df99e4e95a9d4048de0f242bf
itu-itis21-beyden20 <beyden20@itu.edu.tr>
2023-11-30 19:12:40 +0300

    Update random_user_data.py

--- database/create_db/random_user_data.py
+++ database/create_db/random_user_data.py
@@ -12,42 +12,34 @@ def get_random_sport_id():
     result = random.choice([1, 2, 3, 4, 5, 6, 7])
     return result
 
-def get_random_faculty_department():
-    faculty = random.choice(["İnsaat Fakültesi", "Mimarlik Fakültesi", "Makina Fakültesi", "Elektrik-Elektronik Fakültesi", "Denizcilik Fakültesi", "Kimya-Metalurji Fakültesi",
+
+faculty = random.choice(["İnsaat Fakültesi", "Mimarlik Fakültesi", "Makina Fakültesi", "Elektrik-Elektronik Fakültesi", "Denizcilik Fakültesi", "Kimya-Metalurji Fakültesi",
                             "İşletme Fakültesi", "Ucak ve Uzay Bilimleri Fakültesi", "Tekstil Teknolojileri ve Tasarim Fakültesi", "Maden Fakültesi",
                             "Gemi Insaati ve Deniz Bilimleri Fakültesi", "Bilgisayar ve Bilisim Fakültesi", "Fen-Edebiyat Fakültesi"])
-    
 
-    if(faculty = "İnsaat Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Mimarlik Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Makina Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Makina Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Elektrik-Elektronik Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Denizcilik Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Kimya-Metalurji Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "İşletme Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Ucak ve Uzay Bilimleri Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Tekstil Teknolojileri ve Tasarim Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Maden Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Gemi Insaati ve Deniz Bilimleri Fakültesi"):
-        department = random.choice([])
-    elif(faculty = "Bilgisayar ve Bilisim Fakültesi"):
-        department = random.choice([])
-    else:
-        #fen edebiyat bölümleri
-        department = random.choice([])
-    return faculty, department
+faculty_dict = {
+    "İnşaat Fakültesi" : np.random.choice(["İnşaat Mühendisliği", "Geomatik Mühendisliği", "Çevre Mühendisliği"]),
+    "Mimarlık Fakültesi" : np.random.choice(["Mimarlık", "Şehir ve Bölge Planlama", "Endüstri Ürünleri Tasarımı", "Endüstriyel Tasarım", "İç Mimarlık", "Peyzaj Mimarlığı"]),
+    "Makina Fakültesi" : np.random.choice(["Makina Mühendisliği", "İmalat Mühendisliği"]),
+    "Elektrik-Elektronik Fakültesi" : np.random.choice(["Elektronik ve Haberleşme Mühendisliği", "Elektrik Mühendisliği", "Elektronik Mühendisliği", "Telekomünikasyon Mühendisliği", "Kontrol ve Otomasyon Mühendisliği"]),
+    "Maden Fakültesi" : np.random.choice(["Jeoloji Mühendisliği", "Jeofizik Mühendisliği", "Maden Mühendisliği", "Petrol ve Doğal Gaz Mühendisliği", "Cevher Hazırlama Mühendisliği"]),
+    "Kimya - Metalurji Fakültesi" : np.random.choice(["Kimya Mühendisliği", "Gıda Mühendisliği", "Metalurji ve Malzeme Mühendisliği"]),
+    "İşletme Fakültesi" : np.random.choice(["İşletme Mühendisliği", "Endüstri Mühendisliği", "Ekonomi"]),
+    "Gemi İnşaatı ve Deniz Bilimleri Fakültesi" : np.random.choice(["Gemi ve Deniz Teknolojisi Mühendisliği", "Gemi İnşaatı ve Gemi Makineleri Mühendisliği"]),
+    "Fen - Edebiyat Fakültesi" : np.random.choice(["Matematik Mühendisliği", "Fizik Mühendisliği", "Kimya", "Moleküler Biyoloji ve Genetik"]),
+    "Uçak ve Uzay Bilimleri Fakültesi" : np.random.choice(["Meteoroloji Mühendisliği", "Uzay Mühendisliği", "Uçak Mühendisliği"]),
+    "Denizcilik Fakültesi" : np.random.choice(["Deniz Ulaştırma İşletme Mühendisliği", "Gemi Makinaları İşletme Mühendisliği"]),
+    "Tekstil Teknolojileri ve Tasarımı Fakültesi" : np.random.choice(["Tekstil Mühendisliği"]),
+    "Bilgisayar ve Bilişim Fakültesi" : np.random.choice(["Bilgisayar Mühendisliği", "Yapay Zeka ve Veri Mühendisliği"])
+    }
+
+def get_random_faculty_department(faculty, faculty_dict):
+
+    fac = faculty
+    dep = faculty_dict[fac]
+    
+    
+    return fac, dep
 
 
 

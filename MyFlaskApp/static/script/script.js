dfae9d7388de139891dbf8c23c18336c93f46c5d
oykueren <e.oyku35@gmail.com>
2023-12-02 20:38:59 +0300

    Html code is integrated with Flask

---
+++ MyFlaskApp/static/script/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var loginForm = document.getElementById('login-form');
+    var registerForm = document.getElementById('register-form');
+
+    document.getElementById('show-register').addEventListener('click', function() {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'block';
+    });
+
+    document.getElementById('show-login').addEventListener('click', function() {
+        registerForm.style.display = 'none';
+        loginForm.style.display = 'block';
+    });
+
+    document.getElementById('homepage-login').addEventListener('click', function() {
+        window.location.href = "./../";
+    });
+
+    document.getElementById('homepage-register').addEventListener('click', function() {
+        window.location.href = "./../";
+    });
+});
